00042db6b6d28bc88f939d6452136bd5635d7aaa
Alexandru <alexandru@twm.ro>
2026-09-25 11:31:44 +0300

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover">
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    {{-- Not black-translucent: on iOS 26+ it makes the system blur the top of the installed app over the header. --}}
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
     <meta name="apple-mobile-web-app-title" content="FIFA League">
     <meta name="theme-color" content="#05070d">
     <link rel="manifest" href="/manifest.json">
